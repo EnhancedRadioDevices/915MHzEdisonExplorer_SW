85a2fbe7da9bcfe75f42856062d62a4ca8648ef3
morgan <morgan@enhancedradio.com>
2016-09-06 01:21:45 +0000

    ExplorerCtl.py now sends and receives spi data correctly

--- Examples/WirelessSerial/ExplorerCtl.py
+++ Examples/WirelessSerial/ExplorerCtl.py
@@ -4,6 +4,15 @@ import spi_serial
 # TODO: create commands to control the CC1110
 
 if __name__ == "__main__":
+    ss = spi_serial.SpiSerial()
+
     #TODO: get stdin and handle it
     #TODO: push stdout
-    pass
+    while True:
+        try:
+            cmd = raw_input()
+            ss.write([int(c) for c in cmd])
+            if ss.inWaiting() > 0:
+               print(ss.read(0))
+        except:
+            break
