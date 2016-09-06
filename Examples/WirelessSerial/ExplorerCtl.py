bd340c5961be28b0565ff23da00290b35298bb74
morgan <morgan@enhancedradio.com>
2016-09-06 19:10:57 +0000

    updating examples

--- Examples/WirelessSerial/ExplorerCtl.py
+++ Examples/WirelessSerial/ExplorerCtl.py
@@ -1,17 +1,15 @@
 
 import spi_serial
 
-# TODO: create commands to control the CC1110
-
 if __name__ == "__main__":
     ss = spi_serial.SpiSerial()
 
-    #TODO: get stdin and handle it
-    #TODO: push stdout
     while True:
         try:
             cmd = raw_input()
-            ss.write([int(c) for c in cmd])
+            cmd = [int(c) for c in cmd.split(',')]
+            if cmd[0] != 0:
+                ss.write(cmd)
             if ss.inWaiting() > 0:
                print(ss.read(0))
         except:
