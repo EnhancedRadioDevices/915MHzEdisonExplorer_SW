dc3c35884f4fe91c57c4d045e5b6d0fa67887929
Scott <scottleibrand@gmail.com>
2016-09-26 20:57:00 -0700

    syntax

--- spi_serial/setup.py
+++ spi_serial/setup.py
@@ -7,6 +7,6 @@ setup(name="spi_serial",
       url="https://github.com/EnhancedRadioDevices/915MHzEdisonExplorer_SW",
       author="Morgan Redfield",
       author_email="morgan@enhancedradio.com",
-      packages=["spi_serial"]
+      packages=["spi_serial"],
       scripts=['scripts/reset_spi_serial.py']
       )
